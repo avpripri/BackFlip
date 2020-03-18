10fd7705c62a37c5367932aff0ab8016ef8111b1
Bruce <meabru@gmail.com>
2020-03-18 17:39:51 -0500

    Initial Commit

---
+++ BackFlip/ADHRS.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackFlip
+{
+    public class ADHRS : IDisposable
+    {
+        static SerialPort _serialPort;
+
+        public bool IsOpen { get { return _serialPort.IsOpen; } }
+
+        public ADHRS(string comPort, int baudRate = 9600)
+        {
+            _serialPort = new SerialPort(comPort, baudRate);
+
+            try { _serialPort.Open(); } catch (System.IO.IOException x) { }
+        }
+
+        public const char Flags = 'F';
+        public const char Roll = 'R';
+        public const char Pitch = 'P';
+        public const char Heading = 'H';
+        public const char Gs = 'G';
+        public const char Yaw = 'Y';
+        public const char Baro = 'B';
+        public const char Temp = 'T';
+        public const char IAS = 'A';
+
+        public Dictionary<char, float> RawRead()
+        {
+            while (IsOpen && _serialPort.BytesToRead > 0)
+            {
+                try
+                {
+                    var lin = _serialPort.ReadLine();
+                    var ahrsLine = lin.Split(',').Skip(1).Select(v => v.Split(':')).ToDictionary(k => k[0][0], v => float.Parse(v[1]));
+
+                    if (ahrsLine.Count() >= 8)
+                    {
+                        return ahrsLine;
+                    }
+                }
+                catch { break; }
+            }
+
+            return new Dictionary<char, float>();
+        }
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _serialPort.Close();
+                    _serialPort.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+
+        public static int readAltitude(float pressure, float seaLevelhPa)
+        {
+            return (int)(/* meters 44330 */145439.6f * (1.0f - Math.Pow(pressure / seaLevelhPa, 0.1903f)));
+        }
+    }
+}
---
+++ BackFlip/Chevrons3.cs
@@ -0,0 +1,77 @@
+﻿using SharpDX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace BackFlip
+{
+    public class Chevrons3
+    {
+        /// <summary>
+        /// The boundary of the unrotated chevron control
+        /// </summary>
+        public Size2F Size { get; set; }
+
+        /// <summary>
+        /// Maximum angle of attach aka... Alpha  This is also the clean-configuration stall angle of attack
+        /// </summary>
+        public int alphaMax { get; set; }
+
+        /// <summary>
+        /// The ideal angle of attack for the current flight conditions 
+        /// </summary>
+        public int alphaTarget { get; set; }
+
+        // Used internally
+        private struct ChevStruct { public float x0; public float y0; public float y1; }
+
+
+        static readonly Vector4 Red =   new Vector4(1.0f, 0.0f, 0.0f, 1.0f);
+        static readonly Vector4 Green = new Vector4(0.0f, 1.0f, 0.0f, 1.0f);
+        static readonly Vector4 Blue =  new Vector4(0.0f, 0.0f, 1.0f, 1.0f);
+        static readonly Vector4[] Colors = new [] { Blue, Green, Red };
+
+        internal Vector4[] Make()
+        {
+            var vanishingX = Size.Width * 3.0f;
+
+            var width_2 = Size.Width / 2.0f;
+
+            var dyTarget = (float)((alphaMax-alphaTarget) * Size.Height / alphaMax);
+            var m0 = dyTarget / (width_2 + vanishingX);
+            var m1 = (Size.Height - dyTarget) / (width_2/2 + vanishingX);
+
+            var dx = 0.1f; //  Size.Height / Size.Width / 2;
+            var z = -1.0f;
+
+            var pts = new[] { 0f, dyTarget, Size.Height }.Select(y => new { y, mChev = (float)(dyTarget - y) / (float)(width_2 + vanishingX) })
+                .Select(pt => new ChevStruct { x0 = (float)(width_2 - (Size.Height - pt.y) * dx), y0 = pt.y - dyTarget, y1 = pt.y + (width_2 * pt.mChev) - dyTarget })
+                .ToArray();
+
+            // Construct a simple array of all the verticies w/colors in the AoA chevrons (there are 8 total)
+            var rightSides = Enumerable.Range(0, 3).SelectMany(i => new[] { new Vector4(pts[i].x0, pts[i].y0, z, 1.0f), Colors[i] });
+            var leftSides  = Enumerable.Range(0, 3).SelectMany(i => new[] { new Vector4(-pts[i].x0, pts[i].y0, z, 1.0f), Colors[i] });
+            var verticies = rightSides.Concat(leftSides)
+                .Concat(new[] {
+                        new Vector4(0f, pts[0].y1,  z, 1.0f), Colors[0] ,  // Middles
+                        new Vector4(0f, pts[2].y1,  z, 1.0f), Colors[2] }).ToArray();
+
+            // This is the ordered list of vertex that assemble into the triangle list that makes up the chevron
+            return new[] {
+                6,1,0,
+                7,2,1,
+                3,4,6,
+                5,7,4,
+                6,4,1,
+                4,7,1,
+            }.SelectMany(i => new[] { verticies[i * 2], verticies[i * 2 + 1] }).ToArray();
+        }
+        public static Vector4 HorizontalFlip(Vector4 vect)
+        {
+            var arr = vect.ToArray();
+            arr[0] *= -1;
+            return new Vector4(arr);
+        }
+    }
+}
